23e855f048093a41993ba523c99db8abd23b89a7
adeeba0807 <haroon.adeeba0807@gmail.com>
2024-04-28 12:04:18 +0530

    add project

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'X-RapidAPI-Key': '73fffa1367msh50594bcc9b80909p1d80a4jsnf4b54c84fa30',
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
